c85131a4fa4750a8592a9eb9c396fb08debe235e
Boris Klabucar <boris.klabucar@njuskalo.hr>
2023-12-07 20:51:43 +0100

    Add logging

--- start.sh
+++ start.sh
@@ -3,4 +3,6 @@
 gunicorn 'app.main:app' \
     --bind ${ADDRESS:-0.0.0.0}:${PORT:-8000} \
     --workers ${WORKERS:-1} \
-    --worker-class uvicorn.workers.UvicornWorker
+    --worker-class uvicorn.workers.UvicornWorker \
+    --log-level info \
+    --access-logfile="-"
